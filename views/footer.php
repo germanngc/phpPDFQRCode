2fbff59a3617a0cb8a180f1fd7306eff8949b86d
Ismael Ochoa <ismaelocho@gmail.com>
2021-03-26 00:03:55 -0600

    Exeption Column Checkbox Export

--- views/footer.php
+++ views/footer.php
@@ -77,7 +77,10 @@
 					dom: 'Bfrtip',
 					buttons: [
 						$.extend( true, {}, buttonCommon, {
-							extend: 'excelHtml5'
+							extend: 'excelHtml5',
+							exportOptions: {
+								columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ]
+							}
 						} )
 					]
 				} );
